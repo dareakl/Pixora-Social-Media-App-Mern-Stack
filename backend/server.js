35f89410d58008ab0277627e00c0dd39301e83e0
Sathish Raja <dareakl@users.noreply.github.com>
2025-03-25 17:41:30 +0800

    created DB on mongo atlas cloude and connected the DB in application

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => {
+    console.log("DB Connected Successfully");
+  })
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 
