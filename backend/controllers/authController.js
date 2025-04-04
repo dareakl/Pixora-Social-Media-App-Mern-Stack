d4bc86b8a8c2454a2bfdbcc7516b6d61852a949a
Sathish Raja <dareakl@users.noreply.github.com>
2025-04-04 18:22:41 +0800

    resend otp functionality addee

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -111,3 +111,53 @@ exports.verifyAccount = catchAsync(async (req, res, next) => {
 
   createSendToken(user, 200, res, "Email has been verified");
 });
+
+exports.resendOtp = catchAsync(async (req, res, next) => {
+  const { email } = req.user;
+  if (!email) {
+    return next(new AppError("Email is required", 400));
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next(new AppError("User not found", 400));
+  }
+
+  if (user.isVerified) {
+    return next(new AppError("This account is already verified", 400));
+  }
+
+  const otp = generateOtp();
+  const otpExpires = Date.now() + 24 * 60 * 60 * 1000;
+
+  user.otp = otp;
+  user.otpExpires = otpExpires;
+
+  await user.save({ validateBeforeSave: false });
+
+  const htmlTemplate = loadTemplate("otpTemplate.hbs", {
+    title: "Otp Verification",
+    username: user.username,
+    otp,
+    message: "Your one-time password (OTP) for account verification is : ",
+  });
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Resend otpfor email verification",
+      html: htmlTemplate,
+    });
+    res.status(200).json({
+      status: "success",
+      message: "A new Otp is send to your email",
+    });
+  } catch (error) {
+    user.otp = undefined;
+    user.otpExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new AppError("There is an error sending email, Try again later!", 500)
+    );
+  }
+});
